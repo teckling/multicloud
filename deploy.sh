f99730712a27457550a86b8b6b53b0c38092ff40
ttan <ttan@softwareag.com>
2024-09-11 23:39:27 +0800

    re-organize input output files

--- deploy.sh
+++ deploy.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 export SCRIPT_ROOT_DIRECTORY=$(cd ${0%/*} && pwd -L )
 
-source $SCRIPT_ROOT_DIRECTORY/deployment_code_library/aws.lib
-source $SCRIPT_ROOT_DIRECTORY/deployment_code_library/azure.lib
-source $SCRIPT_ROOT_DIRECTORY/deployment_code_library/google.lib
-source $SCRIPT_ROOT_DIRECTORY/deployment_code_library/alibaba.lib
+source $SCRIPT_ROOT_DIRECTORY/deployment_code_library/aws_lib.sh
+source $SCRIPT_ROOT_DIRECTORY/deployment_code_library/azure_lib.sh
+source $SCRIPT_ROOT_DIRECTORY/deployment_code_library/google_lib.sh
+source $SCRIPT_ROOT_DIRECTORY/deployment_code_library/alibaba_lib.sh
 
 PRODUCT=$1
 CLOUD=$2
@@ -12,5 +12,15 @@ REGION=$3
 TENANT=$4
 MODE=$5
 
-TENANT_ENV_FOLDER=tenant_environment/$1/$2/$3/$4
-echo 'deploying resources defined in ' $SCRIPT_ROOT_DIRECTORY/$TENANT_ENV_FOLDER
+#define specific tenant folder
+TENANT_ENV_VARIABLE_SPECIFICATION=tenant_environment/$1/$2/$3/$4
+
+#each tenant environment desired configuration is stored in tenant_environment_input_specification.cfg
+source $SCRIPT_ROOT_DIRECTORY/$TENANT_ENV_VARIABLE_SPECIFICATION/tenant_environment_input_specification.cfg
+
+#each tenant environment existing infrastructure setup is stored in tenant_environment_resulting_state.output
+source $SCRIPT_ROOT_DIRECTORY/$TENANT_ENV_VARIABLE_SPECIFICATION/tenant_environment_resulting_state.output
+
+echo 'deploying additional resources defined in ' $SCRIPT_ROOT_DIRECTORY/$TENANT_ENV_VARIABLE_SPECIFICATION/tenant_environment_input_specification.cfg
+
+create_vpc
